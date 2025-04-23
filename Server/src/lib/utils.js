aa4199a6899f2653ce50df41dc3df6189fe697c2
vishnu-git-repo <vishnu.itofficial@gmail.com>
2025-04-23 16:28:30 +0530

    changing the token options sameSite: lax=>none

--- Server/src/lib/utils.js
+++ Server/src/lib/utils.js
@@ -9,7 +9,7 @@ export const generateToken = (userId,res)=>{
     res.cookie("token",token, {
         maxAge: 14 * 24 * 60 * 60 * 1000, 
         httpOnly: true,
-        sameSite: "lax", 
+        sameSite: "none", 
         secure: process.env.NODE_ENV !== "development", 
     })
     return token;
